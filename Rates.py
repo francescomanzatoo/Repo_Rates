d667ffe064fdc38a0358b6cf8706efbb6fc24409
francescomanzatoo <francyman4@gmail.com>
2026-08-05 19:23:24 +0200

    rates.py optimization
    End of program rates.py by optimizing  the critical steps and functions and adding a last interactive what-if scenario that allow the user to insert his percentuale change values and see the effects on the memorized data

--- Rates.py
+++ Rates.py
@@ -28,25 +28,23 @@ def ratesInsert():
 
 #computing of the exposure: for all the currency, sum of all the value
 def currencyExposure():
-    cursor.execute("""SELECT SUM(amount), currency 
+    cursor.execute("""SELECT SUM(amount) AS amount, currency 
                    FROM positions 
                    GROUP BY currency""")
     espTotValue = cursor.fetchall()
-    cursor.execute("""SELECT currency, SUM(amount), description 
+    cursor.execute("""SELECT currency, SUM(amount) AS amount, description 
                    FROM positions 
                    GROUP BY currency, description""")
     espDescValue = cursor.fetchall()
     print("\nTotal esposition for all currency: ")
     for row in espTotValue:
-        print(row)
-    print("")
-    print("Total esposition for all currency and description: ")
+        print(f"{row["currency"]}: {row["amount"]}")
+    print("\nTotal esposition for all currency and description: ")
     for row in espDescValue:
-        print(row)
+        print(f"{row["currency"]}: {row["amount"]} ({row["description"]})")
 
 #computing of the equivalent value from USD, GBP in EUR
 def equivalentValue():
-    print("")
     cursor.execute("""SELECT p.currency, 
                    SUM(p.amount) AS expVal, 
                    ROUND(SUM(p.amount / r.exchange)) AS eqvVal 
@@ -55,12 +53,14 @@ def equivalentValue():
                    ON r.currency = p.currency AND r.data = (SELECT MAX(data) FROM rates) 
                    GROUP BY p.currency""")
     eqvValue = cursor.fetchall()
-    print("Equivalent value for currency in EUR: ")
+    print("\nEquivalent value for currency in EUR: ")
     for row in eqvValue:
-        print(row)
+        currency = row["currency"]
+        valEUR = row["expVal"]
+        valCurr = row["eqvVal"]
+        print(f"{currency}: {valEUR} EUR correspond to {valCurr} {currency}")
 
-def what_if(currency, percentuale_change):
-    print("")
+def what_if(currency, percentage_change):
     cursor.execute("""SELECT exchange FROM rates
                    WHERE currency = ?
                    AND data = (SELECT MAX(data) FROM rates)""", (currency,))
@@ -68,18 +68,19 @@ def what_if(currency, percentuale_change):
     cursor.execute("""SELECT SUM(amount) FROM positions
                    WHERE currency = ?""", (currency,))
     amount = cursor.fetchone()[0]
-    ifRate = cacheRate * (1 + int(percentuale_change)/100)
+    ifRate = cacheRate * (1 + int(percentage_change)/100)
     actualValue = amount / cacheRate
     ifEqvVal = amount / ifRate
     deltaVar = ifEqvVal - actualValue
     
-    print(f"{currency}: if the rate value changes by {percentuale_change}%,"
-          f" the equivalent value goes from {actualValue: .2f} to {ifEqvVal: .2f}."
+    print(f"\n{currency}: if the rate value changes by {percentage_change}%,"
+          f" the equivalent value goes from{actualValue: .2f} to{ifEqvVal: .2f}."
           f"\nThe variation is: {deltaVar:+.2f}")
     return deltaVar
 
 #connection with the database and definition of a cursor to manipulate it
 connection = sqlite3.connect("fx_dashboard.db")
+connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
 #creation of the database's tables
@@ -113,7 +114,7 @@ if (cursor.fetchone()[0] == 0): positionsInsert()
 cursor.execute("SELECT * FROM positions")
 rows = cursor.fetchall()
 for row in rows:
-   print(row)
+   print(f"{row["ID"]}: {row["currency"]}, {row["amount"]}, {row["data"]}, {row["description"]}")
 
 cursor.execute("SELECT MAX(data) FROM rates")
 lastDateChange = cursor.fetchone()[0]
@@ -126,13 +127,31 @@ if (date.today().isoformat() != lastDateChange):
 else: print("\nExchange rate has not changed from [" + lastDateChange + "]:")
 
 #Showing the most recent date
-cursor.execute("""SELECT currency, MAX(data), exchange FROM rates 
+cursor.execute("""SELECT currency, MAX(data) AS data, exchange FROM rates 
                 GROUP BY currency""")
 rows = cursor.fetchall()
 for row in rows:
-   print(row)
+   print(f"{row["currency"]}: {row["data"]}, {row["exchange"]}")
 
 currencyExposure()
 equivalentValue()
 ifCurrency = 'USD'
 what_if(ifCurrency, 3)
+
+print("\nWhat-if scenario with input rate and currency")
+while 1:
+    print("\nPress Q to exit")
+    newCurr = str(input(f"Insert the currency ({currencyAvailable})\n").strip().upper())
+    if(newCurr == 'Q'): break
+    elif(newCurr not in currencyAvailable):
+        print(f"\nThe allowed currency are only {currencyAvailable}, try again")
+        continue
+    try:
+        newPercentage = float(input("Insert the change percentage\n"))
+    except (ValueError):
+        print("Value not allowed, try again")
+        continue
+    if (newPercentage > 1000 or newPercentage < -1000):
+        print("\nUnrealistic Percentual change, try again")
+    else:
+        what_if(newCurr, newPercentage)
