daa7541a154dc2cab65847c17bc31bc11d333923
francescomanzatoo <francyman4@gmail.com>
2026-08-05 16:17:01 +0200

    exp, eqValue, whatIf
    Updating the core code by some important changes and modifies and implementing the three main function: computing the currency exposure, equivalentValue and what-if scenario, based on the data in the DB. Some important features missing yet.

--- Rates.py
+++ Rates.py
@@ -25,9 +25,58 @@ def ratesInsert():
                        VALUES (?, ?, ?)""", 
                        (row["quote"], row["date"], row["rate"]))
     connection.commit()    
+
+#computing of the exposure: for all the currency, sum of all the value
+def currencyExposure():
+    cursor.execute("""SELECT SUM(amount), currency 
+                   FROM positions 
+                   GROUP BY currency""")
+    espTotValue = cursor.fetchall()
+    cursor.execute("""SELECT currency, SUM(amount), description 
+                   FROM positions 
+                   GROUP BY currency, description""")
+    espDescValue = cursor.fetchall()
+    print("\nTotal esposition for all currency: ")
+    for row in espTotValue:
+        print(row)
+    print("")
+    print("Total esposition for all currency and description: ")
+    for row in espDescValue:
+        print(row)
+
+#computing of the equivalent value from USD, GBP in EUR
+def equivalentValue():
+    print("")
+    cursor.execute("""SELECT p.currency, 
+                   SUM(p.amount) AS expVal, 
+                   ROUND(SUM(p.amount / r.exchange)) AS eqvVal 
+                   FROM positions AS p 
+                   JOIN rates AS r 
+                   ON r.currency = p.currency AND r.data = (SELECT MAX(data) FROM rates) 
+                   GROUP BY p.currency""")
+    eqvValue = cursor.fetchall()
+    print("Equivalent value for currency in EUR: ")
+    for row in eqvValue:
+        print(row)
+
+def what_if(currency, percentuale_change):
+    print("")
+    cursor.execute("""SELECT exchange FROM rates
+                   WHERE currency = ?
+                   AND data = (SELECT MAX(data) FROM rates)""", (currency,))
+    cacheRate = cursor.fetchone()[0]
+    cursor.execute("""SELECT SUM(amount) FROM positions
+                   WHERE currency = ?""", (currency,))
+    amount = cursor.fetchone()[0]
+    ifRate = cacheRate * (1 + int(percentuale_change)/100)
+    actualValue = amount / cacheRate
+    ifEqvVal = amount / ifRate
+    deltaVar = ifEqvVal - actualValue
     
-
-
+    print(f"{currency}: if the rate value changes by {percentuale_change}%,"
+          f" the equivalent value goes from {actualValue: .2f} to {ifEqvVal: .2f}."
+          f"\nThe variation is: {deltaVar:+.2f}")
+    return deltaVar
 
 #connection with the database and definition of a cursor to manipulate it
 connection = sqlite3.connect("fx_dashboard.db")
@@ -51,17 +100,21 @@ cursor.execute("""
         PRIMARY KEY (currency, data)
     )""")
 #valuta, data, tasso
+
+#saving all the DB updates
 connection.commit()
 
-descriptionAvailable = ["fattura fornitore", "Pagamento cliente", "Rimborso spese", 
-                        "Accorto contratto", "Ammortamento immobilizzazioni", "Saldo ordine"]
+descriptionAvailable = ["Supplier invoice", "Client payment", "expense account", 
+                        "Deposit agreement", "Assets amortization", "Sale order"]
 currencyAvailable = ["USD", "GBP"]
-positionsInsert()
+#Adding data to the empty DB
+cursor.execute("SELECT COUNT(*) FROM positions")
+if (cursor.fetchone()[0] == 0): positionsInsert()
+cursor.execute("SELECT * FROM positions")
+rows = cursor.fetchall()
+for row in rows:
+   print(row)
 
-#cursor.execute("SELECT * FROM positions")
-#rows = cursor.fetchall()
-#for row in rows:
-#   print(row)
 cursor.execute("SELECT MAX(data) FROM rates")
 lastDateChange = cursor.fetchone()[0]
 if (date.today().isoformat() != lastDateChange):
@@ -69,9 +122,17 @@ if (date.today().isoformat() != lastDateChange):
                         params = {"base": "EUR", "quotes": "USD,GBP"})
     dataAPI = response.json()
     ratesInsert()
-else: print("Exchange rate has not changed from [" + lastDateChange + "]")
+    print("\nExchange rates has changed:")
+else: print("\nExchange rate has not changed from [" + lastDateChange + "]:")
 
-cursor.execute("SELECT * FROM rates")
+#Showing the most recent date
+cursor.execute("""SELECT currency, MAX(data), exchange FROM rates 
+                GROUP BY currency""")
 rows = cursor.fetchall()
 for row in rows:
    print(row)
+
+currencyExposure()
+equivalentValue()
+ifCurrency = 'USD'
+what_if(ifCurrency, 3)
